89f88fdb62390e2246a351beb713ddf73613e0a9
shadaAhmed47 <115115874+shadaAhmed47@users.noreply.github.com>
2024-03-11 17:00:26 +0200

    Add files via upload

---
+++ first_project/lib/firstfile/first.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+import 'dart:io';
+
+void main(List<String>arguments) {
+ //Task 1
+ //  for(int i=10;i>=0;i--)
+ //    {
+ //      print(i);
+ //    }
+  // Task2
+  // double x= 0.0;
+  // for(int i=0;i<=10;i++)
+  //   {
+  //     x+=.1;
+  //     print (x);
+  //   }
+  //Task3
+  // List<num>nums=[50,60,70,80,90,100];
+  // for(var item in nums){
+  //   print(item);
+  // }
+ //Task 5
+ //  for(int i=1;i<=9;i++)
+ //    {
+ //      for(int j=1;j<=i;j++)
+ //        {
+ //          print('*');
+ //        }
+ //    }
+
+
+
+}
+
+
